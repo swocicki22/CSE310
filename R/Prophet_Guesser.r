752ccfa9774cbbd2ac63f7d3b4a57c08fc27c225
swocicki22 <swocicki@gmail.com>
2023-12-14 21:06:57 -0500

    syntax_error

--- R/Prophet_Guesser.r
+++ R/Prophet_Guesser.r
@@ -23,7 +23,7 @@ get_multiple_choice_input <- function(prompt, choices) {
 play_prophets_game <- function() {
   # Data frame with prophets, birth years, and birthplaces
   prophets_data <- data.frame(
-    Prophet = c("Joseph Smith", "Brigham Young", "Thomas S. Monson", "Russell M. Nelson"),
+    Prophet = c("Joseph Smith", "Brigham Young", "Thomas S. Monson", "Russell M. Nelson")
     BirthYear = c(1805, 1801, 1808, 1927, 1924),
     Birthplace = c("Sharon, Vermont", "Whitingham, Vermont", "Salt Lake City, Utah", "Salt Lake City, Utah")
   )
